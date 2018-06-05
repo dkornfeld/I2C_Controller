7b210f3663c25068a8ba449c61622e4c58cb3e56
aditelik <aditelik@users.noreply.github.com>
2018-06-05 03:30:44 -0700

    Cleaned up code a little bit
    Now only using 5 states.

--- i2c_test/i2c_test.vhd
+++ i2c_test/i2c_test.vhd
@@ -5,7 +5,6 @@ use ieee.std_logic_1164.all;
 entity i2c_test is
 port (
 	clk, start_button, rst:			 	in std_logic;
-   led:                             out std_logic;
 	sda, scl: 								inout std_logic);
 end entity;
 --------------------------------------------------------------------------------
@@ -15,7 +14,7 @@ architecture moore_fsm of i2c_test is
 	type i2c_message is array(natural range <>) of i2c_element;
 
 	--FSM-related declarations:
-	type state is (Waiting, HiA, HiB, LoA, LoB, ReceiveHiA, ReceiveHiB, ReceiveLoA, ReceiveLoB);
+	type state is (Waiting, HiA, HiB, LoA, LoB);
 	signal pr_state, nx_state: state;
 
 	--Timer-related delcarations:
@@ -60,67 +59,59 @@ architecture moore_fsm of i2c_test is
                                                 STOP);
 	constant setup_length: natural := 58;
    constant receive_length: natural := 40;
-	signal i: natural range 0 to setup_length - 1;
-   signal j: natural range 0 to receive_length - 1;
+	signal i: natural range 0 to setup_length;
+   signal j: natural range 0 to receive_length;
    signal stickX: std_logic_vector(7 downto 0);
 
 begin
-	--Timer
+   
+	--Clock and Reset
 	process (clk, rst)
 	begin
-		if rst='0' then -- Active low reset
-			t <= 0;
+      if rst = '0' then
+         t <= 0;
 			i <= 0;
          j <= 0;
+         pr_state <= Waiting;
 		elsif rising_edge(clk) then
-			if pr_state /= nx_state then
+         if pr_state /= nx_state then
 				t <= 0;
-				if (nx_state = LoB or nx_state = Waiting) and j = 0 then
-					i <= i+1;
-				end if;
-            if (nx_state = ReceiveLoB or nx_state = Waiting) and i >= 57 then
-               if pr_state /= HiB and j < receive_length - 1 then
-                  j <= j +1 ;
+            if nx_state = LoB or nx_state = Waiting then
+               if i < setup_length then
+                  i <= i + 1;
+               elsif j < receive_length then
+                  j <= j + 1;
                end if;
-				end if;
+            end if;
 			elsif t /= tmax then
 				t <= t + 1;
 			end if;
-		end if;
-	end process;
-
-	--FSM state register:
-	process (clk, rst)
-	begin
-		if rst='0' then -- Active low reset
-			pr_state <= Waiting;
-		elsif rising_edge(clk) then
-			pr_state <= nx_state;
+         pr_state <= nx_state;
 		end if;
 	end process;
 
 	--FSM combinational logic:
 	process (all)
-		--variable i: natural range 0 to setup_length - 1;
+		variable internal_element: i2c_element;
 		begin
 		case pr_state is
 			when Waiting =>
-				led <= '0';
             scl <= '1';
 				sda <= 'Z';
             if j >= receive_length - 2 or t < T2 - 1 then
                nx_state <= Waiting;
             else
-               if i >= 57 then
-                  nx_state <= ReceiveHiA;
-               else
-                  nx_state <= HiA;
-               end if;
+               nx_state <= HiA;
             end if;
 				
 			when HiA =>
 				scl <= '1';
-				case setup(i) is
+            if i < setup_length then
+               internal_element := setup(i);
+            else
+               internal_element := receive(j);
+            end if;
+				case internal_element is
 					when START => 	sda <= 'Z';
 					when ONE => 	sda <= 'Z';
 					when ZERO => 	sda <= '0';
@@ -130,6 +121,9 @@ begin
 					when RD => 		sda <= 'Z';
 					when others => sda <= 'Z';
 				end case;
+            if j >= 10 and j <= 17 then
+               stickX(j - 10) <= sda;
+            end if;
 				if t >= T1-1 then
 					nx_state <= HiB;
 				else
@@ -138,7 +132,12 @@ begin
 				
 			when HiB =>
 				scl <= '1';
-				case setup(i) is
+            if i < setup_length then
+               internal_element := setup(i);
+            else
+               internal_element := receive(j);
+            end if;
+				case internal_element is
 					when START => 	sda <= '0';
 					when ONE => 	sda <= 'Z';
 					when ZERO => 	sda <= '0';
@@ -149,7 +148,7 @@ begin
 					when others => sda <= 'Z';
 				end case;
 				if t >= T1-1 then
-					if setup(i) = STOP then
+					if internal_element = STOP then
 						nx_state <= Waiting;
 					else
 						nx_state <= LoA;
@@ -160,7 +159,12 @@ begin
 				
 			when LoA =>
 				scl <= '0';
-				case setup(i) is
+            if i < setup_length then
+               internal_element := setup(i);
+            else
+               internal_element := receive(j);
+            end if;
+				case internal_element is
 					when START => 	sda <= '0';
 					when ONE => 	sda <= 'Z';
 					when ZERO => 	sda <= '0';
@@ -178,7 +182,12 @@ begin
 				
 			when LoB =>
 				scl <= '0';
-				case setup(i) is
+            if i < setup_length then
+               internal_element := setup(i);
+            else
+               internal_element := receive(j);
+            end if;
+				case internal_element is
 					when START => 	sda <= 'Z';
 					when ONE => 	sda <= 'Z';
 					when ZERO => 	sda <= '0';
@@ -192,90 +201,7 @@ begin
 					nx_state <= HiA;
 				else
 					nx_state <= LoB;
-				end if;	
-            
-         when ReceiveHiA =>
-				led <= '1';
-            scl <= '1';
-				case receive(j) is
-					when START => 	sda <= 'Z';
-					when ONE => 	sda <= 'Z';
-					when ZERO => 	sda <= '0';
-					when S_ACK => 	sda <= 'Z';
-					when M_ACK => 	sda <= '0';
-					when STOP => 	sda <= '0';
-					when RD => 		sda <= 'Z';
-					when others => sda <= 'Z';
-				end case;
-            if j >= 10 and j <= 17 then
-               stickX(j - 10) <= sda;
-            end if;
-				if t >= T1-1 then
-					nx_state <= ReceiveHiB;
-				else
-					nx_state <= ReceiveHiA;
 				end if;
-				
-			when ReceiveHiB =>
-            led <= '1';
-				scl <= '1';
-				case receive(j) is
-					when START => 	sda <= '0';
-					when ONE => 	sda <= 'Z';
-					when ZERO => 	sda <= '0';
-					when S_ACK => 	sda <= 'Z';
-					when M_ACK => 	sda <= '0';
-					when STOP => 	sda <= '0';
-					when RD => 		sda <= 'Z';
-					when others => sda <= 'Z';
-				end case;
-				if t >= T1-1 then
-					if receive(j) = STOP then
-						nx_state <= Waiting;
-					else
-						nx_state <= ReceiveLoA;
-					end if;
-				else
-					nx_state <= ReceiveHiB;
-				end if;
-				
-			when ReceiveLoA =>
-				led <= '1';
-            scl <= '0';
-				case receive(j) is
-					when START => 	sda <= '0';
-					when ONE => 	sda <= 'Z';
-					when ZERO => 	sda <= '0';
-					when S_ACK => 	sda <= 'Z';
-					when M_ACK => 	sda <= '0';
-					when STOP => 	sda <= 'Z';
-					when RD => 		sda <= 'Z';
-					when others => sda <= 'Z';
-				end case;
-				if t >= T1-1 then
-					nx_state <= ReceiveLoB;
-				else
-					nx_state <= ReceiveLoA;
-				end if;
-				
-			when ReceiveLoB =>
-				led <= '1';
-            scl <= '0';
-				case receive(j) is
-					when START => 	sda <= 'Z';
-					when ONE => 	sda <= 'Z';
-					when ZERO => 	sda <= '0';
-					when S_ACK => 	sda <= 'Z';
-					when M_ACK => 	sda <= '0';
-					when STOP => 	sda <= '0';
-					when RD => 		sda <= 'Z';
-					when others => sda <= 'Z';
-				end case;
-				if t >= T1-1 then
-					nx_state <= ReceiveHiA;
-				else
-					nx_state <= ReceiveLoB;
-				end if;	
             
 		end case;
 	end process;
