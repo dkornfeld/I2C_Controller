3cbe23e928e9c5462ee7af3a23f0be2e375cc2ec
aditelik <aditelik@users.noreply.github.com>
2018-06-05 05:43:50 -0700

    Need to fix issue with STOP sequence.
    Reading may not work...

--- i2c_test/i2c_test.vhd
+++ i2c_test/i2c_test.vhd
@@ -1,10 +1,92 @@
 --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.all;
+--------------------------------------------------------------------------------
+entity bin2bcd_12bit is
+    Port ( binIN : in  STD_LOGIC_VECTOR (11 downto 0);
+           ones : out  STD_LOGIC_VECTOR (3 downto 0);
+           tens : out  STD_LOGIC_VECTOR (3 downto 0);
+           hundreds : out  STD_LOGIC_VECTOR (3 downto 0);
+           thousands : out  STD_LOGIC_VECTOR (3 downto 0)
+          );
+end bin2bcd_12bit;
+
+architecture Behavioral of bin2bcd_12bit is
+
+begin
+
+bcd1: process(binIN)
+
+  -- temporary variable
+  variable temp : STD_LOGIC_VECTOR (11 downto 0);
+  
+  -- variable to store the output BCD number
+  -- organized as follows
+  -- thousands = bcd(15 downto 12)
+  -- hundreds = bcd(11 downto 8)
+  -- tens = bcd(7 downto 4)
+  -- units = bcd(3 downto 0)
+  variable bcd : UNSIGNED (15 downto 0) := (others => '0');
+
+  -- by
+  -- https://en.wikipedia.org/wiki/Double_dabble
+  
+  begin
+    -- zero the bcd variable
+    bcd := (others => '0');
+    
+    -- read input into temp variable
+    temp(11 downto 0) := binIN;
+    
+    -- cycle 12 times as we have 12 input bits
+    -- this could be optimized, we do not need to check and add 3 for the 
+    -- first 3 iterations as the number can never be >4
+    for i in 0 to 11 loop
+    
+      if bcd(3 downto 0) > 4 then 
+        bcd(3 downto 0) := bcd(3 downto 0) + 3;
+      end if;
+      
+      if bcd(7 downto 4) > 4 then 
+        bcd(7 downto 4) := bcd(7 downto 4) + 3;
+      end if;
+    
+      if bcd(11 downto 8) > 4 then  
+        bcd(11 downto 8) := bcd(11 downto 8) + 3;
+      end if;
+    
+      -- thousands can't be >4 for a 12-bit input number
+      -- so don't need to do anything to upper 4 bits of bcd
+    
+      -- shift bcd left by 1 bit, copy MSB of temp into LSB of bcd
+      bcd := bcd(14 downto 0) & temp(11);
+    
+      -- shift temp left by 1 bit
+      temp := temp(10 downto 0) & '0';
+    
+    end loop;
+ 
+    -- set outputs
+    ones <= STD_LOGIC_VECTOR(bcd(3 downto 0));
+    tens <= STD_LOGIC_VECTOR(bcd(7 downto 4));
+    hundreds <= STD_LOGIC_VECTOR(bcd(11 downto 8));
+    thousands <= STD_LOGIC_VECTOR(bcd(15 downto 12));
+  
+  end process bcd1;            
+  
+end architecture;
+--------------------------------------------------------------------------------
+--############################################################################--
+--------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 --------------------------------------------------------------------------------
 entity i2c_test is
 port (
 	clk, start_button, rst:			 	in std_logic;
+   c_led, z_led:                    out std_logic;
 	sda, scl: 								inout std_logic);
 end entity;
 --------------------------------------------------------------------------------
@@ -47,7 +129,7 @@ architecture moore_fsm of i2c_test is
                                                 ONE, ZERO, ONE, ZERO, ZERO, ONE, ZERO, -- 0x52
                                                 ZERO, -- Write
                                                 S_ACK,
-                                                ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, -- 0x00
+                                                ZERO, ZERO, ZERO, ZERO, ZERO, ONE, ZERO, ONE, -- 0x05
                                                 S_ACK,
                                                 STOP,
                                                 START,
@@ -62,9 +144,24 @@ architecture moore_fsm of i2c_test is
 	signal i: natural range 0 to setup_length;
    signal j: natural range 0 to receive_length;
    signal stickX: std_logic_vector(7 downto 0);
-
-begin
    
+   --Display signals
+   signal stickXOnes: std_logic_vector(3 downto 0);
+   signal stickXTens: std_logic_vector(3 downto 0);
+   signal stickXHuns: std_logic_vector(3 downto 0);
+   signal stickXThou: std_logic_vector(3 downto 0);
+   
+   component bin2bcd_12bit is
+    port ( binIN : in  STD_LOGIC_VECTOR (11 downto 0);
+           ones : out  STD_LOGIC_VECTOR (3 downto 0);
+           tens : out  STD_LOGIC_VECTOR (3 downto 0);
+           hundreds : out  STD_LOGIC_VECTOR (3 downto 0);
+           thousands : out  STD_LOGIC_VECTOR (3 downto 0)
+          );
+   end component bin2bcd_12bit;
+   
+begin
+
 	--Clock and Reset
 	process (clk, rst)
 	begin
@@ -79,8 +176,10 @@ begin
             if nx_state = LoB or nx_state = Waiting then
                if i < setup_length then
                   i <= i + 1;
-               elsif j < receive_length then
+               elsif j < receive_length - 1 then
                   j <= j + 1;
+               else
+                  j <= 0;
                end if;
             end if;
 			elsif t /= tmax then
@@ -98,7 +197,7 @@ begin
 			when Waiting =>
             scl <= '1';
 				sda <= 'Z';
-            if j >= receive_length - 2 or t < T2 - 1 then
+            if t < T2 - 1 then
                nx_state <= Waiting;
             else
                nx_state <= HiA;
@@ -121,8 +220,8 @@ begin
 					when RD => 		sda <= 'Z';
 					when others => sda <= 'Z';
 				end case;
-            if j >= 10 and j <= 17 then
-               stickX(j - 10) <= sda;
+            if j >= 30 and j <= 37 then
+               stickX(j - 30) <= sda;
             end if;
 				if t >= T1-1 then
 					nx_state <= HiB;
@@ -206,7 +305,9 @@ begin
 		end case;
 	end process;
    
-   
+   --Display
+   z_led <= stickX(0);
+   c_led <= stickX(1);
    
 end architecture;
 --------------------------------------------------------------------------------
